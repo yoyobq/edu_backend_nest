6435aef1ed933e78d2c1bd628f1a4ed95eca32db
Alex <yoyobq@hotmail.com>
2025-06-08 00:33:09 +0800

    feat(graphql): 添加GraphQL订阅支持
    - 添加graphql-ws和ws依赖以支持GraphQL订阅功能
    - 配置GraphQLModule启用订阅和graphiql界面

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,17 @@ import { CatsModule } from './cats/cats.module';
 @Module({
   imports: [
     // GraphQLModule 类型有延迟推断，实际使用安全，在 strict 模式下压制 ESLint
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: true,
+      graphiql: true,
+      introspection: true, // 可选：启用内省
+      subscriptions: {
+        // graphql-ws 是 Apollo 要求的关键字，不能改名
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'graphql-ws': true, // 开启 graphql-ws 支持
+      },
     }),
     CatsModule,
   ],
