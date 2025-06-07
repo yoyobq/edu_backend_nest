3661121ff0a8181c44b69fde79b8d3cc8a94bdc0
Alex <yoyobq@hotmail.com>
2025-06-08 00:03:27 +0800

    feat(graphql): 添加GraphQL支持及相关依赖
    - @nestjs/graphql和@apollo/server
    - 在app.module中配置GraphQL模块并引入CatsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // GraphQLModule 类型有延迟推断，实际使用安全，在 strict 模式下压制 ESLint
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
