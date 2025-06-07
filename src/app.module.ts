3f762a22f68d23aa80adcfc827b410d06ca55f86
Alex <yoyobq@hotmail.com>
2025-06-08 00:50:14 +0800

    feat(graphql): 添加GraphQL schema文件并更新模块配置
    - 更新AppModule以使用生成的schema文件并启用字段排序

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
-    // GraphQLModule 类型有延迟推断，实际使用安全，在 strict 模式下压制 ESLint
-
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      sortSchema: true, // 排序字段以便 diff
       graphiql: true,
       introspection: true, // 可选：启用内省
       subscriptions: {
