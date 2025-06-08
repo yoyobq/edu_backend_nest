6062e96458960ed7ad238836df511cdd46d47574
Alex <yoyobq@hotmail.com>
2025-06-08 11:03:00 +0800

    feat(graphql): 更新 Apollo Server 配置以启用 Sandbox
    -移除 graphiql 并配置 Apollo Sandbox 作为新的 GraphQL 开发工具

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -11,9 +12,10 @@ import { CatsModule } from './cats/cats.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
-      sortSchema: true, // 排序字段以便 diff
-      graphiql: true,
       introspection: true, // 可选：启用内省
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()], // ✅ 启用 Apollo Sandbox
+      sortSchema: true, // 排序 schema.graphql 中的字段以便 diff
       subscriptions: {
         // graphql-ws 是 Apollo 要求的关键字，不能改名
         // eslint-disable-next-line @typescript-eslint/naming-convention
