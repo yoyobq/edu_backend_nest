57725236affa3eeec4a162aa8ccd7ec77a3a973f
Alex <yoyobq@hotmail.com>
2025-07-22 22:26:57 +0800

    fix(eslint): 放宽 e2e 测试文件的类型检查规则
    - 在 e2e 测试文件中，类型安全不是主要目标，因此禁用部分严格的类型检查规则以提高测试灵活性

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -81,4 +81,29 @@ export default tseslint.config(
       ],
     },
   },
+  // 在 e2e 测试文件中，TS 类型安全不是目标，也不可能让用户指定 类型安全，因此 eslint 类型规则应放宽
+  {
+    files: ['test/**/*.ts'],
+    rules: {
+      // 允许将 any 赋值给其他类型
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      // 允许对 any 类型进行成员访问
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      // 允许函数超过 100 行
+      'max-lines-per-function': 'off',
+      // 允许对 any 类型进行调用
+      '@typescript-eslint/no-unsafe-call': 'off',
+      // 允许不安全的参数
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      // 允许对 any 类型进行索引
+      '@typescript-eslint/no-unsafe-indexing': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      // 允许使用 ！ 进行非空断言
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      // 允许导出的函数和方法不显式声明返回类型
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      // 允许在测试中使用 console.log
+      'no-console': 'off',
+    },
+  },
 );
