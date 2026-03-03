7031e5c641c652a213da83a702d5213a5332cb21
Alex <yoyobq@hotmail.com>
2026-03-04 00:58:05 +0800

    refactor: 移除 ConfigModule 依赖，将 audience 验证移至 AuthService

--- src/usecases/integration-events/integration-events-usecases.module.ts
+++ src/usecases/integration-events/integration-events-usecases.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { INTEGRATION_EVENTS_TOKENS } from '@src/modules/common/integration-events/events.tokens';
 import { EnrollmentCreatedHandler } from '@src/modules/common/integration-events/handlers/enrollment-created.handler';
 import { IntegrationEventsModule } from '@src/modules/common/integration-events/integration-events.module';
 import { OutboxDispatcher } from '@src/modules/common/integration-events/outbox.dispatcher';
 
 @Module({
-  imports: [ConfigModule, IntegrationEventsModule],
+  imports: [IntegrationEventsModule],
   providers: [
     {
       provide: INTEGRATION_EVENTS_TOKENS.HANDLERS,
